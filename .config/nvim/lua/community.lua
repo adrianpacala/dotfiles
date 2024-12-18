6bcae918e4b7be5788290115745cabd11c30a8ec
Adrian Pacala <adrian@pacala.uk>
2024-12-18 20:28:22 +0000

    Clean up astronvim config

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -1,23 +1,4 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.completion.copilot-cmp" },
-  { import = "astrocommunity.docker.lazydocker" },
-  { import = "astrocommunity.fuzzy-finder.telescope-zoxide" },
-  { import = "astrocommunity.motion.leap-nvim" },
-  { import = "astrocommunity.motion.nvim-surround" },
-  { import = "astrocommunity.pack.bash" },
-  { import = "astrocommunity.pack.docker" },
-  { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.pack.json" },
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.ruby" },
-  { import = "astrocommunity.pack.sql" },
-  { import = "astrocommunity.pack.tailwindcss" },
-  { import = "astrocommunity.pack.terraform" },
-  { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.pack.xml" },
-  { import = "astrocommunity.pack.yaml" },
 }
